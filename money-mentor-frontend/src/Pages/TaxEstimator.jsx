91b2eeb110f1ffa062e64acde00bdaaa1acff96a
SatvikPrabhu <satvikp.567@gmail.com>
2026-01-11 02:51:53 +0530

    Update TaxEstimator.jsx

--- money-mentor-frontend/src/Pages/TaxEstimator.jsx
+++ money-mentor-frontend/src/Pages/TaxEstimator.jsx
@@ -5,12 +5,14 @@ import IntroductionToFeature from "../Components/IntroductionToFeature";
 import AgeSlider from "../Components/TaxEstimator/AgeSlider";
 import ResidentialSwitch from "../Components/TaxEstimator/ResidentialSwitch";
 import FinancialYearDropdown from "../Components/TaxEstimator/FYDropdown";
+import SalarySlider from "../Components/TaxEstimator/SalarySlider";
 
 function TaxEstimator() {
     const formSectionRef = useRef(null); 
     const [ageGroup, setAgeGroup] = useState(1);
     const [isNRI, setIsNRI] = useState(false);
     const [financialYear, setFinancialYear] = useState('2024-2025');
+    const[salary, setSalary] = useState(500000);
 
     const scrollToForm = () => {
         formSectionRef.current?.scrollIntoView({ 
@@ -52,47 +54,6 @@ function TaxEstimator() {
     />
     <Box>
         {/* TAXES THEORY */}
-        <Box sx={{ textAlign: 'center', mb: 6 , mt:10}}>
-            <Typography variant="h3" sx={{ fontWeight: 'bold', color: 'text.primary', mb: 2 }}>
-                Understanding Income Tax
-            </Typography>
-            <Typography variant="h6" sx={{ color: 'text.primary', opacity: 0.8 }}>
-                Everything you need to know about tax calculations in India
-            </Typography>
-        </Box>
-
-        <Box sx={{ 
-            color: 'text.primary',
-            pl: 10,
-            pr:10,
-            pt:5, 
-            borderRadius: 2, 
-            mt:4,
-            mb: 4,
-            boxShadow: 2
-        }}>
-            <Typography variant="h4" sx={{ fontWeight: 'bold', mb: 3 }}>
-            What is the new tax regime?
-            </Typography>
-            <Typography sx={{fontSize:20}}>
-                The New Tax Regime is a simplified income tax system introduced by the Government of India to make tax calculation easier and more transparent. It offers lower tax slab rates compared to the Old Tax Regime but removes most exemptions and deductions.
-                <br/>
-                <br/>
-                From FY 2023-24 onwards, the New Tax Regime is the <strong>default</strong> tax regime for individual taxpayers. However, taxpayers can still choose the Old Tax Regime if it is more beneficial for them.
-            </Typography>
-            {/* Old Regime */}
-            <Box sx={{ mb: 4, mt:2 }}>
-                <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 2 }}>
-                    Slabs
-                </Typography>
-                <Typography variant="body1" sx={{ mb: 2 }}>
-                    Tax slabs are predefined income ranges, where each range is taxed at a specific rate.
-                    <br/>
-                    Your entire income is not taxed at one single rate. Instead, different portions of your income are taxed at different rates as you move through the slabs.
-                </Typography>
-            {/* Add table or list of tax slabs here */}
-            </Box>
-        </Box>
     </Box>
     <Box 
         ref={formSectionRef}  
@@ -144,6 +105,11 @@ function TaxEstimator() {
             <Typography variant="h5" sx={{ mb: 2, fontWeight: 'bold' }}>
                 Income & Deductions
             </Typography>
+
+            <Box sx={{ mb: 3 }}>
+            <SalarySlider value={salary} onChange={setSalary} />
+            </Box>
+            
             
         </Box>
     </Box>
