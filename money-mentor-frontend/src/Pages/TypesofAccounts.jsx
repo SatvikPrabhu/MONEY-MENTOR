456a8fe7347242bb9139908dba50509f77c58e77
regenpalkar28 <regenrahuleenapalkar@gmail.com>
2026-01-21 23:21:38 +0530

    Made changes

--- money-mentor-frontend/src/Pages/TypesofAccounts.jsx
+++ money-mentor-frontend/src/Pages/TypesofAccounts.jsx
@@ -1,8 +1,8 @@
-import HomeIcon from '@mui/icons-material/Home';
-import SchoolIcon from '@mui/icons-material/School';
+import SavingsIcon from '@mui/icons-material/Savings';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import PersonIcon from '@mui/icons-material/Person';
 import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 import LoanPageCard from "../Components/LoanPage/LoanPageCard";
 
 function TypesofAccounts()
@@ -14,11 +14,11 @@ function TypesofAccounts()
             <h1 className="loans-h1">TYPES OF ACCOUNTS</h1>
         </div>
         <div className="loan-cards">
-            <LoanPageCard icon={<HomeIcon sx={{fontSize:70}}/>} title="SAVINGS ACCOUNT" para="for everyday saving and spending"/>
-            <LoanPageCard icon={<SchoolIcon sx={{fontSize:70}}/>} title="CURRENT ACCOUNT" para="for businesses and frequent transactions"/>
+            <LoanPageCard icon={<SavingsIcon sx={{fontSize:70}}/>} title="SAVINGS ACCOUNT" para="for everyday saving and spending"/>
+            <LoanPageCard icon={<AccountBalanceIcon sx={{fontSize:70}}/>} title="CURRENT ACCOUNT" para="for businesses and frequent transactions"/>
             <LoanPageCard icon={<PersonIcon sx={{fontSize:70}}/>} title="FIXED DEPOSIT" para="deposit money for a fixed period"/>
             <LoanPageCard icon={<TimeToLeaveIcon sx={{fontSize:70}}/>} title="RECURRING DEPOSIT" para="save small amounts every month"/>
-            <LoanPageCard icon={<AutoAwesomeIcon sx={{fontSize:70}}/>} title="SALARY ACCOUNT" para="account for salaried individuals"/>
+            <LoanPageCard icon={<EngineeringIcon sx={{fontSize:70}}/>} title="SALARY ACCOUNT" para="account for salaried individuals"/>
         </div>
         </div>
     )
